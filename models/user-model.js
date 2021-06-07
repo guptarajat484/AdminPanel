ae6470e4c1d2cf618bd82f83e3d8cde0957806fd
guptarajat484 <gupta.rajat484@gmail.com>
2021-06-07 19:17:26 +0530

    changes done

---
+++ models/user-model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const user = new mongoose.Schema({
+  name: {
+    type: String,
+    lowercase: true,
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    trim: true,
+  },
+  phone: {
+    type: Number,
+    trim: true,
+  },
+  password: {
+    type: String,
+    trim: true,
+  },
+  invitecode: {
+    type: String,
+    trim: true,
+  },
+  isadmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("User", user);
--- src/controller/auth-controller.js
+++ src/controller/auth-controller.js
@@ -1,12 +1,10 @@
 const joi = require("joi");
 const bcrypt = require("bcrypt");
 const User = require("../../models/user-model");
-const auth=require('../../middlewares/auth-middleware')
+const auth = require("../../middlewares/auth-middleware");
 function signup(req, res) {
-  const { name, email, password, conformpassword, role, mobilenumber } =
-    req.body;
-
-  console.log(typeof role);
+  const { name, email, password, cpassword, isadmin, phone } = req.body;
+  
 }
 
 function login(req, res) {}
