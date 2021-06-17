f666682586cc588e0c20d5d7353bf443a2da8ff5
guptarajat484 <gupta.rajat484@gmail.com>
2021-06-17 13:46:03 +0530

    modifymiddleware

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,12 +1,15 @@
 const jwt = require("jsonwebtoken");
 
 function authentication(req, res, next) {
-  const token = req.header(auth);
+  const token = req.header("auth");
   if (!token) {
-    return res.status(422).send({ error: "Unauthorized Access" });
+    return res.status(422).send({ error: "Invalid Token" });
   }
   try {
-    const decoded = jwt.decode(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return res.status(422).send({ Error: "Unauthorized Access" });
+    }
     req.user = decoded;
     next();
   } catch (err) {
